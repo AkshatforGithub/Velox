14eafe9d02c972b01a32c8a14fed35948db2b876
Akshat Kumar <foronlinework345@gmail.com>
2025-03-05 18:49:53 +0530

    added axios and useAuth

---
+++ frontend/src/store/useAuth.js
@@ -0,0 +1,25 @@
+import {create} from 'zustand';
+import { instance } from '../lib/axios';
+
+export const useAuth = create((set) => ({
+    authUser: null,
+    isCheckingAuth: true,
+    isSigningUp: false,
+    isLoggingIng: false,
+    isUpdatingProfile: false,
+
+// authenticate the user using already made check funtion in the backend
+    checkAuth: async () => {
+        try {
+            const res = await instance.get("/auth/check");
+
+            set({authUser: res.data});
+
+// if the user is not found meaning there is no auth user meaning set the checking to false meaning user does not exist 
+        } catch (error) {
+         set({authUser: null});   
+        }finally{
+            set({isCheckingAuth: false});
+        }
+    }
+}));
